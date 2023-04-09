dc509dfffa15cb78398e1338e05a46104afb6700
teeramail <44755428+teeramail@users.noreply.github.com>
2023-04-09 08:33:45 +0700

    Update qrbox.vue

--- pages/qrbox.vue
+++ pages/qrbox.vue
@@ -1,21 +1,24 @@
 <template>
-    <div>
-      <ClientOnly>
-        <qrcode-stream @decode="onDecode" @error="onError"></qrcode-stream>
-      </ClientOnly>
-    </div>
-  </template>
-  
-  <script setup>
-  import { QrcodeStream } from "vue3-qrcode-reader";
-  
-  const onDecode = (content) => {
-    console.log("Decoded QR code:", content);
-  };
-  
-  const onError = (error) => {
-    console.error("QR code scanning error:", error);
-  };
-  </script>
-  
-  
+  <div>
+    <ClientOnly>
+      <qrcode-stream @decode="onDecode" @error="onError"></qrcode-stream>
+    </ClientOnly>
+    <div v-if="decodedContent">Decoded content: {{ decodedContent }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { QrcodeStream } from "vue3-qrcode-reader";
+
+const decodedContent = ref(null);
+
+const onDecode = (content) => {
+  console.log("Decoded QR code:", content);
+  decodedContent.value = content;
+};
+
+const onError = (error) => {
+  console.error("QR code scanning error:", error);
+};
+</script>
