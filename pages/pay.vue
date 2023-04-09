bef4bfea31132428e3cec729c21221d01c2723f8
teeramail <44755428+teeramail@users.noreply.github.com>
2023-04-09 08:04:45 +0700

    Add files via upload

---
+++ pages/pay.vue
@@ -0,0 +1,37 @@
+<template>
+    <form
+      id="payment-form"
+      action="https://cdn.XYhillpay.co/Payment/"
+      method="post"
+      role="form"
+      class="form-horizontal"
+    >
+      <modernpay:widget
+        id="modernpay-widget-container"
+        data-merchantid="M033348"
+        data-amount="10000"
+        data-orderno="00000001"
+        data-customerid="123456"
+        data-mobileno="0889999999"
+        data-clientip="58.11.97.209"
+        data-routeno="1"
+        data-currency="764"
+        data-description="Test Payment"
+        data-apikey="AXXY8ELcfizFvI9vh9EWsvCwz1SmlZammV52DAFfo6zPjUd1Z6"
+      ></modernpay:widget>
+      <button type="submit" id="btnSubmit" value="Submit" class="btn">Payment</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://cdn.chillpay.co/js/widgets.js?v=1.00";
+      script.charset = "utf-8";
+      document.body.appendChild(script);
+    },
+  };
+  </script>
+  
---
+++ pages/qrbox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <ClientOnly>
+        <qrcode-stream @decode="onDecode" @error="onError"></qrcode-stream>
+      </ClientOnly>
+    </div>
+  </template>
+  
+  <script setup>
+  import { QrcodeStream } from "vue3-qrcode-reader";
+  
+  const onDecode = (content) => {
+    console.log("Decoded QR code:", content);
+  };
+  
+  const onError = (error) => {
+    console.error("QR code scanning error:", error);
+  };
+  </script>
+  
+  
