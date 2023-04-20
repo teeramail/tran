4def1fef736ce675bd7706d030cf3a45fd82fd1c
teeramail <44755428+teeramail@users.noreply.github.com>
2023-04-20 16:19:02 +0700

    Update getresultpay.vue

--- pages/getresultpay.vue
+++ pages/getresultpay.vue
@@ -1,11 +1,10 @@
-// pages/getresultpay
-
+// pages/getresultpay.vue
 <template>
-    <div>
-      <h1>Request Data</h1>
-      <pre>{{ JSON.stringify(requestData, null, 2) }}</pre>
-    </div>
- </template>
+  <div>
+    <h1>Request Data</h1>
+    <pre>{{ JSON.stringify(requestData, null, 2) }}</pre>
+  </div>
+</template>
 
 <script setup>
 import { ref } from 'vue';
@@ -13,10 +12,14 @@ import { useAsyncData } from '#app';
 
 const requestData = ref({});
 
-useAsyncData('requestData', async (context) => {
-  const { req } = context;
-  requestData.value = req.body;
+useAsyncData('requestData', async () => {
+  const response = await fetch('/api/getStoredData');
+
+  if (response.ok) {
+    const data = await response.json();
+    requestData.value = data;
+  } else {
+    console.error('Error fetching data');
+  }
 });
 </script>
-
-
