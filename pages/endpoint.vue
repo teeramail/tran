0c4c4f5a03cc0671782eb844bac4a1f5b0cebcb0
teeramail <44755428+teeramail@users.noreply.github.com>
2023-04-21 09:40:04 +0700

    Add files via upload

--- pages/endpoint.vue
+++ pages/endpoint.vue
@@ -1,19 +1,23 @@
+//  pages/endpoint.vue
+
 <template>
   <div>
-    <!-- Display the transNo value -->
     <p>transNo: {{ transNo }}</p>
+    <pre>{{ requestData }}</pre>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
 const transNo = ref(null)
+const requestData = ref(null)
 
-const fetchtransNo = async () => {
-  const { req } = useNuxt().ssrContext
-  const data = req.body.transNo
-  return { transNo: data }
+const fetchTransNo = async () => {
+  const response = await fetch('/postRequestHandler')
+  const data = await response.json()
+  return { transNo: data.transNo, requestData: data }
 }
 
-useAsyncData('transNo', fetchtransNo, transNo)
+useAsyncData('transNo', fetchTransNo, { transNo, requestData })
 </script>
