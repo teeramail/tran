ba626cd46c9038684051fdbac7b50dbf65ad11e7
teeramail <44755428+teeramail@users.noreply.github.com>
2023-04-21 09:40:30 +0700

    Add files via upload

--- server-middleware/postRequestHandler.js
+++ server-middleware/postRequestHandler.js
@@ -1,14 +1,16 @@
+// server-middleware/postRequestHandler.js
+
 const querystring = require('querystring');
 
 module.exports = function (req, res, next) {
-    let body = '';
+  let body = '';
 
-    req.on('data', (data) => {
-        body += data;
-    });
+  req.on('data', (data) => {
+    body += data;
+  });
 
-    req.on('end', () => {
-        req.body = querystring.parse(body) || {};
-        next();
-    });
+  req.on('end', () => {
+    req.body = querystring.parse(body) || {};
+    res.end(JSON.stringify(req.body)); // Send the entire req.body to the client
+  });
 };
