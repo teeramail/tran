ced681d5d48c7d6112a1b32a316e2b9d683f2eb3
teeramail <44755428+teeramail@users.noreply.github.com>
2023-05-02 07:49:51 +0700

    Add files via upload

--- pages/qrbox.vue
+++ pages/qrbox.vue
@@ -4,6 +4,7 @@
       <qrcode-stream @decode="onDecode" @error="onError"></qrcode-stream>
     </ClientOnly>
     <div v-if="decodedContent">Decoded content: {{ decodedContent }}</div>
+    <div v-if="result">Result: {{ result }}</div>
   </div>
 </template>
 
@@ -12,10 +13,34 @@ import { ref } from 'vue';
 import { QrcodeStream } from "vue3-qrcode-reader";
 
 const decodedContent = ref(null);
+const result = ref(null);
 
-const onDecode = (content) => {
+const onDecode = async (content) => {
   console.log("Decoded QR code:", content);
   decodedContent.value = content;
+  
+  try {
+    const response = await fetch(`https://koh-samui.com:53005/reservations?orderno=${content}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    if (data.orderno === content) {
+      result.value = data;
+    } else {
+      result.value = "Order number not found or doesn't match";
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 };
 
 const onError = (error) => {
