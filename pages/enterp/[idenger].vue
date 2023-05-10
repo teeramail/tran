07bc719bc0c7b01cbd845c25d555b23d95ed8558
teeramail <44755428+teeramail@users.noreply.github.com>
2023-05-10 07:26:30 +0700

    Add files via upload

---
+++ pages/enterp/[idenger].vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const idenger = route.params.idenger;
+
+async function submitForm() {
+  try {
+    const response = await fetch('https://koh-samui.com:53005/enterp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ idenger }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Error adding record: ' + response.statusText);
+    }
+
+    router.push({ path: '/phetchbuncha', query: { idenger: idenger} });
+  } catch (error) {
+    alert('Error adding record: ' + error.message);
+  }
+}
+
+onMounted(() => {
+  submitForm();
+});
+</script>
---
+++ pages/enterp/[idenger]bk.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Enterp - Add New Record</h1>
+    <button @click="submitForm">Submit</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+
+const route = useRoute()
+const idenger = ref('');
+
+async function submitForm() {
+  try {
+    const response = await fetch('https://koh-samui.com:53005/enterp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ idenger: route.params.idenger }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Error adding record: ' + response.statusText);
+    }
+
+    alert('Record added successfully');
+    idenger.value = '';
+  } catch (error) {
+    alert('Error adding record: ' + error.message);
+  }
+}
+</script>
+  
