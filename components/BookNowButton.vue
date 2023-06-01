a2795b08166e0ae0d42fc90db7b82c1e42b45014
teeramail <44755428+teeramail@users.noreply.github.com>
2023-06-01 12:33:30 +0700

    Add files via upload

---
+++ components/BookNowButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button @click="goToReser" class="absolute bottom-1/4 left-1/2 transform -translate-x-1/2 text-white text-2xl bg-green-500 py-4 px-8 rounded-full">Book Now</button>
+
+</template>
+  
+<script setup>
+
+import { useRouter, useRoute } from 'vue-router'
+  
+  const props = defineProps({
+    preserId: {
+      type: [String, null],
+      default: null
+    }
+  })
+
+  const router = useRouter()
+  const route = useRoute()
+  
+  const goToReser = () => {
+    router.push({
+      name: 'event',
+      query: {
+        preserId: props.preserId,
+        idenger: props.idenger,
+       
+      },
+    })
+  }
+</script>
+  
---
+++ components/Enter.vue
@@ -0,0 +1,70 @@
+<template>
+    <div></div>
+</template>
+  
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+// const props = defineProps({
+//     idenger: {
+//         type: String,
+//         required: true,
+//     },
+// });
+
+const emit = defineEmits(['prereservation-created']);
+
+const route = useRoute();
+const router = useRouter();
+const idenger = ref(route.query.idenger); 
+
+async function submitForm() {
+    try {
+        const response = await fetch('https://koh-samui.com:53005/enterp', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ idenger: idenger.value }), 
+        });
+
+        if (!response.ok) {
+            throw new Error('Error adding record: ' + response.statusText);
+        }
+          
+        // Prepare the data to be sent to the server
+        const preReservationData = {
+            idenger: idenger.value,   
+            createdAt: new Date(),
+        }
+  
+        const reservationResponse = await fetch("https://koh-samui.com:53005/prereservation", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(preReservationData)
+        });
+    
+        if (!reservationResponse.ok) {
+            throw new Error(`HTTP error! status: ${reservationResponse.status}`);
+        }
+
+        const data = await reservationResponse.json();
+        const prereservationId = data._id; 
+        console.log(prereservationId);
+        emit('prereservation-created', prereservationId);
+
+        router.push({ path: '/a01', query: { idenger: idenger.value } });
+    } catch (error) {
+        alert('Error adding record: ' + error.message);
+    }
+}
+  
+onMounted(() => {
+    if (idenger.value) { 
+        submitForm();
+    }
+});
+</script>
