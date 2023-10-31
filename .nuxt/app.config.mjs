8a69372729765dc6aa70168ea8ed3fd669e4de44
Tee <teeramail@gmail.com>
2023-10-31 14:27:04 +0700

    Untrack .nuxt and node_modules directories

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/cteer/vueproject/nuxt3/muaythaicm/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/cteer/vueproject/nuxt3/muaythaicm/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
