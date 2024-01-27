5299b8618d8ba3589b99dd20d7584da0e47ab7a5
Tee <teeramail@gmail.com>
2024-01-27 09:51:25 +0700

    1

--- pages/car.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div class="p-4 font-sans">
-    <div class="max-w-2xl mx-auto">
-        <h1 class="text-2xl font-bold mb-4">Koh Phangan, Thongsala, Haad Rin Muay Thai(Thai Boxing)</h1>
-         <up/>
-    </div>
-</div>
-</template>
--- pages/golf.vue
+++
@@ -1,8 +0,0 @@
-// C:\Users\cteer\vueproject\nuxt3\samui\pages\a01.vue
-<template>
-  <div class="container mx-auto flex flex-col items-center">
-  
-  <h1>Golf</h1>
-  </div>
-</template>
-
