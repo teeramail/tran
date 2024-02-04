7dbe628ab0569571f0c573fc8d866092bccf6ddd
Tee <teeramail@gmail.com>
2024-02-04 18:16:54 +0700

    1

--- utils/20Chiangmaiplus.js
+++ utils/20Chiangmaiplus.js
@@ -1,6 +1,6 @@
 const moment = require('moment-timezone');
 
-let startDate = moment.tz('2024-01-29 21:00', 'Asia/Bangkok');
+let startDate = moment.tz('2024-02-05 21:00', 'Asia/Bangkok');
 const events = [];
 
 const numberOfDays = 7;
