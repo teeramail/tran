e1f55d0ba4a132aeaf364385c15b76e38a3eab81
Teera <teeramail@gmail.com>
2025-04-13 16:59:13 +0700

    Initial commit for tranform

--- pages/pay.vue
+++ pages/pay.vue
@@ -17,7 +17,7 @@
         data-routeno="1"
         data-currency="764"
         data-description="Test Payment"
-        data-apikey="Db6Ep74yKoBrsTkEsg8ELcfizFvI9vh9EWsvCwz1SmlZammV52DAFfo6zPjUd1Z6"
+        data-apikey="7ynsXqBl3e0vFPfI1fivU9VSAZ8UZTQmta7vz4b6heptCXrrEja8ub1Z8YW6VnDX"
       ></modernpay:widget>
       <button type="submit" id="btnSubmit" value="Submit" class="btn">Payment</button>
     </form>
